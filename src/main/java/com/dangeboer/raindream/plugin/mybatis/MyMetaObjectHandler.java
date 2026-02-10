8cc386f85e2c74cddee5740213ddda5c2fd792d1
CYX <dangeboe@gmail.com>
2026-02-10 00:05:39 -0500

    新增：mybatis自动插入/更新

--- src/main/java/com/dangeboer/raindream/plugin/mybatis/MyMetaObjectHandler.java
+++ src/main/java/com/dangeboer/raindream/plugin/mybatis/MyMetaObjectHandler.java
@@ -1,4 +1,31 @@
 package com.dangeboer.raindream.plugin.mybatis;
 
-public class MyMetaObjectHandler {
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+public class MyMetaObjectHandler implements MetaObjectHandler {
+    /**
+     * 新增时自动填充时间的格式
+     *
+     * @param metaObject 元数据
+     */
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        this.strictInsertFill(metaObject, "createdTime", LocalDateTime::now, LocalDateTime.class);
+        this.strictUpdateFill(metaObject, "updatedTime", LocalDateTime::now, LocalDateTime.class);
+    }
+
+    /**
+     * 更新时自动填充时间的格式
+     *
+     * @param metaObject 元数据
+     */
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        this.strictUpdateFill(metaObject, "updatedTime", LocalDateTime::now, LocalDateTime.class);
+    }
 }
