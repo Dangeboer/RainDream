12bc5a231fce877da9fefec8446569be22ea6ae4
CYX <dangeboe@gmail.com>
2026-02-10 15:07:47 -0500

    修改：完善BaseEntity

--- src/main/java/com/dangeboer/raindream/base/BaseEntity.java
+++ src/main/java/com/dangeboer/raindream/base/BaseEntity.java
@@ -6,11 +6,15 @@ import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.io.Serial;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
+@Getter
+@Setter
 // 实现 Serializable 即这个类可以被序列化，方便缓存、网络传输等
 public class BaseEntity implements Serializable {
     // 标识此类，防止类结构变动导致反序列化失败
@@ -26,11 +30,11 @@ public class BaseEntity implements Serializable {
     @TableField(fill = FieldFill.INSERT) // 插入数据时自动填充
     @JsonInclude(value = JsonInclude.Include.NON_NULL) // 如果字段是null就不返回给前端
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    private LocalDateTime createdTime;
+    private LocalDateTime createdAt;
 
     // 更新时间
     @TableField(fill = FieldFill.INSERT_UPDATE) // 插入和更新时都自动填充
     @JsonInclude(value = JsonInclude.Include.NON_NULL)
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    private LocalDateTime updatedTime;
+    private LocalDateTime updatedAt;
 }
