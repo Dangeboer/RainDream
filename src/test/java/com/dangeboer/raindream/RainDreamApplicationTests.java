064718c6a01de57aab901cec4006472a0ec75ffc
Dangeboer <dangeboe@gmail.com>
2026-02-09 19:03:30 -0500

    init

---
+++ src/test/java/com/dangeboer/raindream/RainDreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dangeboer.raindream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RainDreamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
