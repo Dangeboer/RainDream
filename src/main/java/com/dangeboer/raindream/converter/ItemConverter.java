3206b07c53050e3cca39bd7a871572b2cc7744b4
CYX <dangeboe@gmail.com>
2026-02-10 21:37:53 -0500

    修改：Converter利用mapstruct完成label匹配

--- src/main/java/com/dangeboer/raindream/converter/ItemConverter.java
+++ src/main/java/com/dangeboer/raindream/converter/ItemConverter.java
@@ -1,16 +1,26 @@
 package com.dangeboer.raindream.converter;
 
+import com.dangeboer.raindream.model.entity.Fanfic;
 import com.dangeboer.raindream.model.entity.Item;
+import com.dangeboer.raindream.model.form.FanficForm;
 import com.dangeboer.raindream.model.form.ItemForm;
 import com.dangeboer.raindream.model.vo.ItemListVO;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface ItemConverter {
 
     // entity -> listVO
+    @Mapping(
+            target = "trackingTypeLabel",
+            expression = "java(com.dangeboer.raindream.base.IBaseEnum.labelOf(item.getTrackingType(), com.dangeboer.raindream.enums.TrackingTypeEnum.class))"
+    )
     ItemListVO toListVO(Item item);
 
     // form -> entity
     Item toItem(ItemForm itemForm);
+
+    // form -> entity (fanfic)
+    Fanfic toFanfic(FanficForm fanficForm);
 }
