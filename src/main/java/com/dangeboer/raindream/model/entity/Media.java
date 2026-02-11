f9e617471f671f15cbb0dcff8caa5149e94a3928
CYX <dangeboe@gmail.com>
2026-02-10 21:37:21 -0500

    修改：完善Media相关类

--- src/main/java/com/dangeboer/raindream/model/entity/Media.java
+++ src/main/java/com/dangeboer/raindream/model/entity/Media.java
@@ -1,10 +1,12 @@
 package com.dangeboer.raindream.model.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class Media {
     private Long itemId;
     private String thumbUrl;
