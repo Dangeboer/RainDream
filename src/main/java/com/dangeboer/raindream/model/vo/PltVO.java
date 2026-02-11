c132b86181a384e2214ac2aa8b0ca6261f20199b
CYX <dangeboe@gmail.com>
2026-02-11 09:32:54 -0500

    新增：标签和平台VO

---
+++ src/main/java/com/dangeboer/raindream/model/vo/PltVO.java
@@ -0,0 +1,9 @@
+package com.dangeboer.raindream.model.vo;
+
+import java.io.Serializable;
+
+public class PltVO implements Serializable {
+    private Long id;
+    private Long userId;
+    private String pltName;
+}
